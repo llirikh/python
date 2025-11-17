7d5e4a82646cb675f71d97efd1cad34fb866e341
llirikh <zhukov.kg@phystech.edu>
2025-11-17 18:39:17 +0300

    Fix workflow

--- tasks/01_2_basic_types/filter_list_by_list/filter_list_by_list.py
+++
@@ -1,7 +0,0 @@
-def filter_list_by_list(lst_a: list[int] | range, lst_b: list[int] | range) -> list[int]:
-    """
-    Filter first sorted list by other sorted list
-    :param lst_a: first sorted list
-    :param lst_b: second sorted list
-    :return: filtered sorted list
-    """
