cfcefdfcf366c657f5914ba5af93fcfafbae562d
llirikh <zhukov.kg@phystech.edu>
2025-11-17 20:58:41 +0300

    Solve some base_types tasks

--- tasks/01_2_basic_types/filter_list_by_list/filter_list_by_list.py
+++ tasks/01_2_basic_types/filter_list_by_list/filter_list_by_list.py
@@ -5,3 +5,16 @@ def filter_list_by_list(lst_a: list[int] | range, lst_b: list[int] | range) -> l
     :param lst_b: second sorted list
     :return: filtered sorted list
     """
+    i: int = 0
+    j: int = 0
+    lst_result: list[int] = []
+    while i < len(lst_a):
+        if j >= len(lst_b) or lst_a[i] < lst_b[j]:
+            lst_result.append(lst_a[i])
+            i += 1
+        elif lst_b[j] == lst_a[i]:
+            i += 1
+        else:
+            j += 1
+
+    return lst_result
--- tasks/01_2_basic_types/merge_lists/merge_lists.py
+++ tasks/01_2_basic_types/merge_lists/merge_lists.py
@@ -5,7 +5,29 @@ def merge_iterative(lst_a: list[int], lst_b: list[int]) -> list[int]:
     :param lst_b: second sorted list
     :return: merged sorted list
     """
+    lst_result: list[int] = []
+    i: int = 0
+    j: int = 0
+    while i < len(lst_a) or j < len(lst_b):
+        if i >= len(lst_a):
+            lst_result.append(lst_b[j])
+            j += 1
+            continue
 
+        if j >= len(lst_b):
+            lst_result.append(lst_a[i])
+            i += 1
+            continue
+
+        if lst_a[i] <= lst_b[j]:
+            lst_result.append(lst_a[i])
+            i += 1
+            continue
+
+        lst_result.append(lst_b[j])
+        j += 1
+
+    return lst_result
 
 def merge_sorted(lst_a: list[int], lst_b: list[int]) -> list[int]:
     """
@@ -14,3 +36,4 @@ def merge_sorted(lst_a: list[int], lst_b: list[int]) -> list[int]:
     :param lst_b: second sorted list
     :return: merged sorted list
     """
+    return sorted(lst_a + lst_b)
