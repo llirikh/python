0491202f1d1ac268100e6e373d13cc1c17d7f64a
llirikh <zhukov.kg@phystech.edu>
2025-11-17 19:16:57 +0300

    Solve middle_value task

--- tasks/01_2_basic_types/middle_value_of_triple/middle_value_of_triple.py
+++ tasks/01_2_basic_types/middle_value_of_triple/middle_value_of_triple.py
@@ -4,8 +4,6 @@ def get_middle_value(a: int, b: int, c: int) -> int:
     """
     if b <= a <= c or c <= a <= b:
         return a
-    
     if a <= b <= c or c <= b <= a:
         return b
-    
     return c
