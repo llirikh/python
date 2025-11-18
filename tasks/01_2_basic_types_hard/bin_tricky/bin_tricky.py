47e115b51b316f9e968c8e4216f559a94491ddc5
llirikh <zhukov.kg@phystech.edu>
2025-11-18 20:32:49 +0300

    Solve bin_tricky

--- tasks/01_2_basic_types_hard/bin_tricky/bin_tricky.py
+++ tasks/01_2_basic_types_hard/bin_tricky/bin_tricky.py
@@ -9,3 +9,40 @@ def find_median(nums1: Sequence[int], nums2: Sequence[int]) -> float:
     :return: middle value if sum of sequences' lengths is odd
              average of two middle values if sum of sequences' lengths is even
     """
+    m: int = len(nums1)
+    n: int = len(nums2)
+
+    i: int = 0
+    j: int = 0
+
+    prev: int = -1
+    cur: int = -1
+
+    while (i + j) < (m + n) // 2:
+        if j >= n:
+            cur = nums1[i]
+            i += 1
+        elif i >= m:
+            cur = nums2[j]
+            j += 1
+        else:
+            if nums1[i] <= nums2[j]:
+                cur = nums1[i]
+                i += 1
+            else:
+                cur = nums2[j]
+                j += 1
+
+        prev = cur
+
+    if i >= m:
+        cur = nums2[j]
+    elif j >= n:
+        cur = nums1[i]
+    else:
+        cur = min(nums1[i], nums2[j])
+
+    if (n + m) % 2 == 1:
+        prev = cur
+    
+    return (prev + cur) / 2
