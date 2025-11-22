a643df6f5ff888444d8852b1c5724be172818f07
llirikh <zhukov.kg@phystech.edu>
2025-11-22 20:59:39 +0300

    Solve data_structures_hard tasks

--- tasks/02_1_data_structures_hard/alphabet/alphabet.py
+++ tasks/02_1_data_structures_hard/alphabet/alphabet.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
 import enum
+from itertools import zip_longest
+import typing as tp
 
 
 class Status(enum.Enum):
@@ -6,6 +9,27 @@ class Status(enum.Enum):
     EXTRACTED = 1
     FINISHED = 2
 
+def topological_sort(
+        vertex: str,
+        graph: dict[str, set[str]],
+        stack: list[str],
+        is_visited: dict[str, Status]
+        ):
+    """
+    Topological sorting of graph
+    :param vertex: vertex of graph
+    :param graph: graph with partial order
+    :param stack: stack of vertex
+    :param is_visited: flag if vertex is visited
+    """
+    is_visited[vertex] = Status.EXTRACTED
+
+    for v in graph[vertex]:
+        if is_visited[v] == Status.NEW:
+            topological_sort(v, graph, stack, is_visited)
+
+    is_visited[vertex] = Status.FINISHED
+    stack.append(vertex)
 
 def extract_alphabet(
         graph: dict[str, set[str]]
@@ -15,7 +39,15 @@ def extract_alphabet(
     :param graph: graph with partial order
     :return: alphabet
     """
+    stack: list[str] = []
+    is_visited: dict[str, Status] = {v : Status.NEW for v in graph}
 
+    for vertex in graph:
+        if is_visited[vertex] == Status.NEW:
+            topological_sort(vertex, graph, stack, is_visited)
+
+    stack.reverse()
+    return stack
 
 def build_graph(
         words: list[str]
@@ -25,7 +57,29 @@ def build_graph(
     :param words: ordered words
     :return: graph
     """
+    result: tp.DefaultDict[str, set[str]] = defaultdict(set)
 
+    if not words:
+        return result
+
+    if len(words) == 1:
+        for c in words[0]:
+            result[c]
+
+    for i in range(len(words) - 1):
+        ignore_flag = False
+        for left_c, right_c in zip_longest(words[i], words[i + 1]):
+            if left_c is not None:
+                parent = result[left_c]
+
+                if right_c is not None:
+                    if left_c != right_c and (not ignore_flag):
+                        parent.add(right_c)
+                        ignore_flag = True
+
+            if right_c is not None:
+                result[right_c]
+    return result
 
 #########################
 # Don't change this code
--- tasks/02_1_data_structures_hard/banner_search_system/banner_search_system.py
+++ tasks/02_1_data_structures_hard/banner_search_system/banner_search_system.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+import heapq
+import typing as tp
+
 def normalize(
         text: str
         ) -> str:
@@ -6,6 +10,7 @@ def normalize(
     :param text: text to normalize
     :return: normalized query
     """
+    return ''.join(c for c in text if c.isalpha() or c.isspace()).lower()
 
 
 def get_words(
@@ -16,6 +21,7 @@ def get_words(
     :param query: query to split
     :return: filtered and split query by words
     """
+    return [word for word in normalize(query).split() if len(word) > 3]
 
 
 def build_index(
@@ -26,6 +32,16 @@ def build_index(
     :param banners: list of banners for indexation
     :return: mapping from word to banners ids
     """
+    dict_: tp.DefaultDict[str, list[int]] = defaultdict(list[int])
+    for banner_idx, banner_str in enumerate(banners):
+        words: list[str]= get_words(banner_str)
+        for word in words:
+            banner_idx_list = dict_[word]
+            if (not banner_idx_list) or (banner_idx_list[-1] != banner_idx):
+                banner_idx_list.append(banner_idx)
+    
+    return dict_
+
 
 
 def get_banner_indices_by_query(
@@ -38,6 +54,38 @@ def get_banner_indices_by_query(
     :param index: index to search banners
     :return: list of indices of suitable banners
     """
+    words: list[str] = get_words(query)
+
+    ptrs: list[int] = [0] * len(words)
+    heap: list[tuple[int, int]] = []
+    for word_idx, word in enumerate(words):
+        if index[word]:
+            heap.append((index[word][0], word_idx))
+    heapq.heapify(heap)
+
+    cur_idx: int | None = None
+    cur_idx_cnt: int = 0
+
+    result: list[int] = []
+
+    while heap:
+        item, word_idx = heapq.heappop(heap)
+
+        if item == cur_idx:
+            cur_idx_cnt += 1
+        else:
+            cur_idx = item
+            cur_idx_cnt = 1
+        
+        if (cur_idx is not None) and (cur_idx_cnt == len(words)):
+            result.append(cur_idx)
+
+        ptrs[word_idx] += 1
+        word = words[word_idx]
+        if (cur_ptr := ptrs[word_idx]) < len(index[word]):
+            heapq.heappush(heap, (index[word][cur_ptr], word_idx))
+
+    return result
 
 
 #########################
@@ -60,3 +108,14 @@ def get_banners(
     return [banners[i] for i in indices]
 
 #########################
+
+
+INDEX = {
+   "купить": [0, 1, 2, 3, 4, 5, 6],
+   "джинсы": [2, 3, 4],
+   "скидка": [0, 3, 4, 5]
+}
+
+QUERY = "купить 5 джинсы скидка 100р"
+
+LST = get_banner_indices_by_query(QUERY, INDEX)
